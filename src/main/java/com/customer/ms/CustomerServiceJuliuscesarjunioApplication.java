078b84cacd0e71c8aec83de67703751483be2299
juliu <juliuscesarjunio@yahoo.com>
2020-09-29 17:43:50 -0400

    initial commit

---
+++ src/main/java/com/customer/ms/CustomerServiceJuliuscesarjunioApplication.java
@@ -0,0 +1,13 @@
+package com.customer.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceJuliuscesarjunioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceJuliuscesarjunioApplication.class, args);
+	}
+
+}
