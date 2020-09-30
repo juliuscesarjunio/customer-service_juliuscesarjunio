f89d01ce47ab420371b6a5dbe6c84e99789e66d5
juliu <juliuscesarjunio@yahoo.com>
2020-09-30 17:31:10 -0400

    session 2

--- src/main/java/com/customer/ms/controller/CustomerController.java
+++ src/main/java/com/customer/ms/controller/CustomerController.java
@@ -12,12 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.customer.ms.dao.CustomerDAO;
 import com.customer.ms.model.Customer;
+import com.customer.ms.model.CustomerM;
+import com.customer.ms.service.CustomerService;
 
 @RestController
 public class CustomerController {
 	
 	@Autowired
 	private CustomerDAO customerDAO;
+	
+	@Autowired
+	private CustomerService customerService;
 	
 	@RequestMapping("/hello")
 	public String hello() {
@@ -48,6 +53,22 @@ public class CustomerController {
 	@RequestMapping(value = "/customers", method = RequestMethod.PUT, produces = {MediaType.APPLICATION_JSON_VALUE})
 	public Customer deleteCustomer(@RequestBody Customer customer) {
 		return customerDAO.updateCustomer(customer);
+	}
+	
+	@RequestMapping(value = "/mongoCustomers", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public List<CustomerM> getMongoCustomers() {
+		List<CustomerM> list = customerService.findAll();
+		return list;
+	}
+	
+	@RequestMapping(value = "/mongoCustomer/{cusId}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public CustomerM getMongoCustomers(@PathVariable("cusId") String cusId) {
+		return customerService.findById(cusId);
+	}
+	
+	@RequestMapping(value = "/mongoCustomer", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public CustomerM addMongoCustomer(@RequestBody CustomerM customerM) {
+		return customerService.addCustomer(customerM);
 	}
 
 }
